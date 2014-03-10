36168f759f792fef1357b199acecb756d327d2a6
David Dovel <davey5555@gmail.com>
2014-03-10 17:26:29 -0500

    Initial Commit

---
+++ CowardlyRogue/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  CowardlyRogue
+//
+//  Created by David Dovel on 3/10/14.
+//  Copyright QBobble 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ CowardlyRogue/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CowardlyRogue
+//
+//  Created by David Dovel on 3/10/14.
+//  Copyright QBobble 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
